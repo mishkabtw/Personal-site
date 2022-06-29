6a1f6fe33c4c8d888c5baac64f4f2c2a3bceb47c
mishkabtw <98282948+mishkabtw@users.noreply.github.com>
2022-06-29 22:31:09 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+jQuery(document).ready(function($) {
+    
+    $('.level-bar-inner').css('width', '0');
+    
+    $(window).on('load', function() {
+
+        $('.level-bar-inner').each(function() {
+        
+            var itemWidth = $(this).data('level');
+            
+            $(this).animate({
+                width: itemWidth
+            }, 1500);
+            
+        });
+
+    });
+    
+    $('.level-label').tooltip();
+    
+    
+    $("#rss-feeds").rss(
+    
+        "http://feeds.feedburner.com/TechCrunch/startups",
+        
+        {
+        limit: 5,
+        
+        effect: 'slideFastSynced',
+        
+        layoutTemplate: "<div class='item'>{entries}</div>",
+        
+        entryTemplate: '<h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>Read more</a></div>'
+        
+        }
+    );
+    
+    GitHubCalendar("#github-graph", "IonicaBizau");
+    
+    GitHubActivity.feed({ username: "GomerSimpson0", selector: "#ghfeed" });
+
+
+});
